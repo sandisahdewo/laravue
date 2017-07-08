7f6bf94d59a4a55884c14f867a7284d6bbbb9935
sandi sahdewo <sandi.sahdewo@gmail.com>
2017-07-08 15:56:05 +0700

    Change call view in route index

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 Route::middleware('auth')->group(function() {
 	Route::prefix('master')->group(function() {
